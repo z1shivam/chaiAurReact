12ae9ee5528a1191bd2295d8401624d28c842910
z1shivam <shivamk.json@gmail.com>
2023-10-05 18:58:14 +0530

    okay lets commit

---
+++ viteReactProject/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+   <h1>Chai aur React - learning by shivam</h1>
+   <Chai />
+    </>
+  )
+}
+export default App
---
+++ viteReactProject/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h2>chai ban gai hai</h2>
+    )
+}
+
+export default Chai
