21b13eec71137498efedc28711afdb9d728e93cc
z1shivam <shivamk.json@gmail.com>
2023-10-07 14:43:00 +0530

    until hooks completed

--- 01viteReactProject/src/App.jsx
+++ 01viteReactProject/src/App.jsx
@@ -1,9 +1,14 @@
 import Chai from "./chai"
 
+
 function App() {
+  const username = 'shivam kumar'
+  //jsx me final outcome likhte hai, simple variable. saara functioning bahar hoga.
+  // kyunki finally ek object pass hota h or usme hum function ya conditionals nhi likh skte
+  
   return (
     <>
-   <h1>Chai aur React - learning by shivam</h1>
+   <h1>Chai aur React - learning by {username}</h1>
    <Chai />
     </>
   )
