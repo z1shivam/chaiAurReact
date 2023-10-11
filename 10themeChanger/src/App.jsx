a7779d78a90d3d2d89a8fc62c4f7d9b22265411f
z1shivam <shivamk.json@gmail.com>
2023-10-11 14:18:08 +0530

    theme changer built

---
+++ 10themeChanger/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap justify-center h-screen items-center ">
+        <div className="w-full max-w-lg">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto"></div>
+          <Card />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
