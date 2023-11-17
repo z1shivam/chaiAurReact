47268973ce8611eeba19ce345449b1b8cba6b048
z1shivam <shivamk.json@gmail.com>
2023-11-17 19:13:00 +0530

    learnt redux toolkit again

---
+++ 13ReactTooklitTodoAgain/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-red-500'>
+      <Header />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ 13ReactTooklitTodoAgain/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header className='h-16 bg-indigo-500 w-full'>
+
+    </header>
+    </>
+  )
+}
+
+export default Header
